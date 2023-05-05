32411f9e4fd3fbf1d5341956bcf9e1de3338a0b2
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2023-05-05 21:00:32 +0100

    add post rspec tests

--- app/models/like.rb
+++ app/models/like.rb
@@ -7,7 +7,12 @@ class Like < ApplicationRecord
              foreign_key: :post_id,
              class_name: :Post
 
-  after_create :update_posts_counter
-  after_destroy :update_posts_counter
-  after_update :update_posts_counter
+  after_create :update_post_likes_counter
+  after_destroy :update_post_likes_counter
+  after_update :update_post_likes_counter
+
+  def update_post_likes_counter
+    post.likes_counter = post.likes.count
+    post.save
+  end
 end
