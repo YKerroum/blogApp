54deb0188fea40dac8acc6ac272a35620ec3dd6c
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2023-05-05 20:35:25 +0100

    add validations to users and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 250, too_long: '250 characters is the maximum allowed' }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   belongs_to :author,
              foreign_key: :author_id,
              class_name: :User
