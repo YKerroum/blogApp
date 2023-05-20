c35cb0a6ebb204845d5159e08a6a50fbb4cc612b
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2023-05-20 18:46:31 +0100

    update controllers and fiw linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,30 @@ class PostsController < ApplicationController
     @posts = @user.posts
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+    if @post.save
+      flash[:notice] = 'Post created successfully!'
+      redirect_to user_posts_path(current_user)
+    else
+      flash[:alert] = "Couldn't create new post!"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def show
     @post = @user.posts.find(params[:id])
+    @like = Like.new
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
